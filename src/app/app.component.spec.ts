35c93e428bbdb23c214fb2cbd458c0d4bb5f9653
David Kennedy <davek1802@gmail.com>
2018-10-03 15:05:54 -0700

    add declarations and imports to app.component.spec.ts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,19 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { FormsModule }   from '@angular/forms';
+import { CardComponent } from './card/card.component';
+
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        CardComponent
       ],
+      imports: [FormsModule],
     }).compileComponents();
   }));
-  /*
+  
   it('should create the app', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
@@ -25,5 +30,5 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Welcome to Quick Draw!');
   }))
-  */
+  
 });
